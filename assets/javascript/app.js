d73672bbaf501a41b25d06cce96da09c2acd3ee5
Andrew Beckett <Andrew Beckett>
2016-08-06 22:57:06 -0400

    adding stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+	$.ajax({url: queryURL, method: 'GET'})
+	 .done(function(response) {
+	     console.log(response);
+	}); 
